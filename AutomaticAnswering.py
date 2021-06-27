6360e94386fe62db0a4d0b0750a760b5dba245a8
donaldjnnt <47189574+donaldjnnt@users.noreply.github.com>
2021-06-28 04:16:31 +0530

    Update AutomaticAnswering.py

--- AutomaticAnswering.py
+++ AutomaticAnswering.py
@@ -173,7 +173,7 @@ if __name__=='__main__':
         #v2w_model = gensim.models.KeyedVectors.load("./w2vecmodel.mod")
         #print("Loaded w2v model")
         #except:
-        v2w_model = api.load('glove-twitter-25')
+        v2w_model = api.load('semeval-2016-2017-task3-subtaskA-unannotated')
         v2w_model.save("./w2vecmodel.mod")
         print("Saved w2v model")
 
